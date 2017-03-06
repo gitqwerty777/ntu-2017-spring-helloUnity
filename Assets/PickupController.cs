b4d537599849b2ecf426385bd14d0b2b6d14f3de
王瀚宇 <huw12313212@gmail.com>
2017-03-07 04:36:41 +0800

    ＋Project Finished

---
+++ Assets/PickupController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public float RotateSpeed;
+
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.localEulerAngles += new Vector3 (0,0,RotateSpeed*Time.deltaTime);
+		
+	}
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	private int _score = 0;
+	const string ScorePrefix = "Score : ";
+
+	// Use this for initialization
+	void Start () {
+
+		scoreText = this.GetComponent<Text> ();
+		scoreText.text = ScorePrefix + _score;
+	}
+
+	public void AddScore(int scoreAdded)
+	{
+		_score += scoreAdded;
+		scoreText.text = ScorePrefix + _score;
+	}
+
+}
